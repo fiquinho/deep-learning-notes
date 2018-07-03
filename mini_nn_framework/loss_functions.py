2b5e5996110230a703714827afb0a1c54bfb7b3e
Federico Bogado <ficobogado@gmail.com>
2018-07-03 02:12:54 -0300

    Added binary regression loss derivative

--- mini_nn_framework/loss_functions.py
+++ mini_nn_framework/loss_functions.py
@@ -11,3 +11,15 @@ def binary_logistic_regression_loss(labels: np.array, predictions: np.array) -> 
     :return: A matrix with the losses.
     """
     return -(labels * np.log(predictions)) - ((1 - labels) * np.log(1 - predictions))
+
+
+def binary_logistic_regression_loss_prime(labels: np.array, predictions: np.array) -> np.array:
+    """
+    Get the derivatives of the losses for the binary logistic regression task, with respect to
+    the predictions, from a matrix with real labels and a matrix with predictions.
+
+    :param labels: A matrix with the true labels.
+    :param predictions: A matrix with the predictions.
+    :return: A matrix with the derivative of the losses with respect to the predictions.
+    """
+    return -(labels * (1 / predictions)) + ((1 - labels) / (1 - predictions))
